4fca83aba2708e822f1bd99a684e146bb7efadd8
Robin Clowers <robin.clowers@gmail.com>
2013-02-14 17:59:25 -0800

    create empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoSiteBackend::Application.config.secret_token = 'bd66fe24388d9c7c62bd26e5c03c225ffe21bde111c80a3b42791a54f90aa173dc1b11aae4208e8331100345c2168b589675e45a67592d6d0ecae269e5429c06'
